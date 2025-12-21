5ada8a39c6a690cf4a2081f8554bf27ef3623315
Vladislav6410 <vladoexport@gmail.com>
2025-12-21 14:43:06 +0100

    Delete Webkuriercore directory
    Signed-off-by: Vladislav6410 <vladoexport@gmail.com>

--- Webkuriercore/config.py
+++
@@ -1,8 +0,0 @@
-# config.py — настройки бота
-
-# 🔐 Вставь сюда токен от BotFather
-BOT_TOKEN = "telegram-token" content="7145533143:AAEirzSuGNSLwNi4UOILOt7-8EGZ6Z295PM"
-
-# (опционально) можно добавить настройки прокси, логов и т.д.:
-# PROXY_URL = "http://proxy.example.com:3128"
-# ADMIN_ID = 7145533143
