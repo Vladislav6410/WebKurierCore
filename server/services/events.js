3dfe97e6ac48e934bf24ab21094d92c8130d4ace
Vladislav6410 <vladoexport@gmail.com>
2026-01-02 13:51:36 +0100

    Delete server directory
    Signed-off-by: Vladislav6410 <vladoexport@gmail.com>

--- server/services/events.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-export async function track(payload){
-  if(!process.env.MAKE_WEBHOOK_URL) return;
-  try { await axios.post(process.env.MAKE_WEBHOOK_URL, payload); } catch {}
-}
