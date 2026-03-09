2fa0c296d6ab1bdf46bd7af9bb91c1a4174a2828
Vladislav6410 <vladoexport@gmail.com>
2026-03-09 07:07:40 +0100

    mission-panel.js erstellen

---
+++ engine/agents/autopilot/ui/mission-panel.js
@@ -0,0 +1,12 @@
+import {
+  getStatus,
+  getTelemetry,
+  getActionLog,
+  verifyChain,
+  buildMission,
+  validateMission,
+  uploadMission,
+  sendAction,
+  issueMissionCertificate,
+  getMissionCertificate
+} from "../api/mission-api.js";
