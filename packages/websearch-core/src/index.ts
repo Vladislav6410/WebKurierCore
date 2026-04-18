fe43dab69270cb4d635075c118573762e617a9a0
Vladislav6410 <vladoexport@gmail.com>
2026-04-18 14:35:45 +0200

    index.ts erstellen

---
+++ packages/websearch-core/src/index.ts
@@ -0,0 +1,9 @@
+export { WebSearchClient } from './WebSearchClient';
+export type { SearchResponse } from './WebSearchClient';
+
+export { SearchMode, SEARCH_MODE_CONFIG } from './types/SearchMode';
+export type {
+  SearchConfig,
+  DomainFilters,
+  UserLocation
+} from './types/SearchConfig';
